c2a271736b50cb5c11f5a0f0d2d901274e2aad91
Sreesanth46 <sreesanth46@gmail.com>
2024-05-01 04:37:59 +0530

    feat: configured env

---
+++ server/config/config.py
@@ -0,0 +1,13 @@
+from pydantic import Field
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    OPEN_WEATHER_API_KEY: str = Field(alias='OPEN_WEATHER_API_KEY')
+    MONGO_DB_URL: str = Field(alias='MONGO_DB_URL')
+
+    model_config = SettingsConfigDict(env_file='.env')
+
+
+# global instance
+settings = Settings()
