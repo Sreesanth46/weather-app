a50f02fd012bf80d9ea3ed188ba31ad6f04cfe3c
Sreesanth46 <sreesanth46@gmail.com>
2024-05-01 17:26:47 +0530

    feat:navigation component with search command

---
+++ client/src/components/navigation/index.tsx
@@ -0,0 +1,12 @@
+import { CommandSearch } from "../command-diaglogue";
+
+const Navigation = () => {
+  return (
+    <nav className="flex w-full items-center justify-between py-4">
+      <div />
+      <CommandSearch />
+    </nav>
+  );
+};
+
+export default Navigation;
