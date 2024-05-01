92c4ca5a303e94018878c908de8628920ea0c2d5
Sreesanth46 <sreesanth46@gmail.com>
2024-05-01 20:38:43 +0530

    feat: added weather listing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,26 @@
+import { useEffect } from "react";
 import SearchDropDown from "./components/search/search";
-import Navigation from "./components/navigation";
+import CurrentWeather from "~/components/current-weather";
+import { listWeather } from "~/lib/fetch-api";
+import { useApiStore } from "./store";
+// import Navigation from "./components/navigation";
 
 function App() {
+  const { weatherList } = useApiStore();
+
+  useEffect(() => {
+    listWeather();
+  }, []);
+
   return (
     <div className="container p-6">
-      <SearchDropDown />
       {/* <Navigation /> */}
+      <SearchDropDown />
+      <div className="pt-6">
+        {weatherList?.map((weather) => (
+          <CurrentWeather key={weather.city} weather={weather} />
+        ))}
+      </div>
     </div>
   );
 }
