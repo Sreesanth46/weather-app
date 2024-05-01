cd69a757e5e1aa5fad488e881086f4164b4ebbc5
Sreesanth46 <sreesanth46@gmail.com>
2024-05-01 16:37:41 +0530

    chore: constants

---
+++ client/src/constants/data-constants.ts
@@ -0,0 +1,27 @@
+import { Location } from "~/types";
+
+export const DEFAULT_LOCATION: Location = {
+  city: "New York",
+  coord: {
+    lat: "40.7128",
+    lon: "74.0060",
+  },
+};
+
+export const DEFAULT_SUGGESTIONS = [
+  {
+    description: "London, United Kingdom",
+  },
+  {
+    description: "Paris, France",
+  },
+  {
+    description: "Amsterdam, Netherlands",
+  },
+  {
+    description: "Dublin, Ireland",
+  },
+  {
+    description: "Lisbon, Portugal",
+  },
+];
