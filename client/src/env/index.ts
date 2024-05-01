256ada137a282a14053ca3f6d2af39ada505900e
Sreesanth46 <sreesanth46@gmail.com>
2024-05-01 18:55:33 +0530

    chore: added backed url

--- client/src/env/index.ts
+++ client/src/env/index.ts
@@ -2,8 +2,10 @@ import z from "zod";
 
 const envSchema = z.object({
   VITE_RAPID_API_KEY: z.string(),
+  VITE_BACKEND_URL: z.string().url(),
 });
 
 export const env = envSchema.parse({
   VITE_RAPID_API_KEY: import.meta.env.VITE_RAPID_API_KEY,
+  VITE_BACKEND_URL: import.meta.env.VITE_BACKEND_URL,
 });
